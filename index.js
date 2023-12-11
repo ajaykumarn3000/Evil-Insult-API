5a3ee99a1bb6d1bd682388f66ef1d5da4021b441
Ajaykumar <ajaykumarn3000@gmail.com>
2023-12-11 13:32:16 +0530

    added views folder

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import axios from "axios";
+import * as path from "path";
+import {fileURLToPath} from "url"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 const port = 3000;
 const url = "https://evilinsult.com/generate_insult.php?lang=en&type=json";
 
 const app = express();
 app.use(express.static("public"));
 app.set('view engine','ejs');
-
+app.set('views', path.join(__dirname, '/views'))
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
