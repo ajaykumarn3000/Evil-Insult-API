54db48e26fb1da003b74af35f2608f52ca51b789
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2023-12-11 13:07:02 +0530

    Update

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const url = "https://evilinsult.com/generate_insult.php?lang=en&type=json";
 const app = express();
 app.use(express.static("public"));
 
-const wallpaperList = [
-  "https://wallpaperaccess.com/full/432699.jpg",
-  "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1124&q=100",
-  "https://wallpaperaccess.com/full/432767.jpg",
-  "https://wallpapercave.com/wp/wp2915636.jpg",
-];
-
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
