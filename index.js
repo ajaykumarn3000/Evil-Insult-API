9d394d06ae54a90501a0436479a4cefc727f8916
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2023-12-11 13:17:49 +0530

    added ejs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const url = "https://evilinsult.com/generate_insult.php?lang=en&type=json";
 
 const app = express();
 app.use(express.static("public"));
+app.set('view engine','ejs');
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
